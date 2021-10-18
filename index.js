3d0d3081dda6c517d790e8d0b367f07f2ed2e503
Gabriel <shalitmontagne.g@gmail.com>
2021-10-18 13:00:35 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData(name, email) {
+    const userData = {
+        name: `${name}`,
+        email: `${email}`
+    };
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+    body: JSON.stringify(userData),
+    };
+
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (object) {
+        let body = document.querySelector('body')
+        let h1 = document.createElement('h1')
+    h1.innerHTML = `${object.id}`;
+    body.appendChild(h1);
+    }).catch(function(error){
+        document.body.innerHTML = error.message;
+    })
+
+}
+
